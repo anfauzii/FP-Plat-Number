a0ddda71ea26b63eb2a51ae874d36a2c45d3b407
anfauzii <ahmadnurfauzi1704@gmail.com>
2023-07-14 00:26:30 +0700

    upload

--- TrainAndTestData.py
+++ TrainAndTestData.py
@@ -6,21 +6,20 @@ import os
 import cv2
 import numpy as np
 
-# module level variables ##########################################################################
+# module level variables
 MIN_CONTOUR_AREA = 100
 RESIZED_IMAGE_WIDTH = 20
 RESIZED_IMAGE_HEIGHT = 30
 
 
-###################################################################################################
+
 class ContourWithData():
-    # member variables ############################################################################
-    npaContour = None  # contour
-    boundingRect = None  # bounding rect for contour
-    intRectX = 0  # bounding rect top left corner x location
-    intRectY = 0  # bounding rect top left corner y location
-    intRectWidth = 0  # bounding rect width
-    intRectHeight = 0  # bounding rect height
+    npaContour = None
+    boundingRect = None
+    intRectX = 0 
+    intRectY = 0  
+    intRectWidth = 0  
+    intRectHeight = 0  
     fltArea = 0.0  # area of contour
 
     def calculateRectTopLeftPointAndWidthAndHeight(self):  # calculate bounding rect info
@@ -31,11 +30,10 @@ class ContourWithData():
         self.intRectHeight = intHeight
 
     def checkIfContourIsValid(self):  # this is oversimplified, for a production grade program
-        if self.fltArea < MIN_CONTOUR_AREA: return False  # much better validity checking would be necessary
+        if self.fltArea < MIN_CONTOUR_AREA: return False 
         return True
 
 
-###################################################################################################
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--image_testing",
@@ -80,14 +78,12 @@ def main():
     imgBlurred = cv2.GaussianBlur(imgGray, (5, 5), 0)  # blur
 
     # filter image from grayscale to black and white
-    imgThresh = cv2.adaptiveThreshold(imgBlurred,  # input image
-                                      255,  # make pixels that pass the threshold full white
+    imgThresh = cv2.adaptiveThreshold(imgBlurred,
+                                      255,
                                       cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                      # use gaussian rather than mean, seems to give better results
                                       cv2.THRESH_BINARY_INV,
-                                      # invert so foreground will be white, background will be black
-                                      11,  # size of a pixel neighborhood used to calculate threshold value
-                                      2)  # constant subtracted from the mean or weighted mean
+                                      11, 
+                                      2) 
 
     imgThreshCopy = imgThresh.copy()  # make a copy of the thresh image, this in necessary b/c findContours modifies the image
 
@@ -153,8 +149,6 @@ def main():
 
     return
 
-
-###################################################################################################
 if __name__ == "__main__":
     main()
 # end if
